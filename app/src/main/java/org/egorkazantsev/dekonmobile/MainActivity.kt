8fd1ea0d2633b57b07b9d570d58b2d1b3d241244
Egor Kazantsev <3gork4z4ncev@gmail.com>
2022-05-18 16:52:49 +0500

    Initial commit

---
+++ app/src/main/java/org/egorkazantsev/dekonmobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.egorkazantsev.dekonmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
