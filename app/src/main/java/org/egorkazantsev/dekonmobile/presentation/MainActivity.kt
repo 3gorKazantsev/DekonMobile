e3c95e707525366564fa022ea482536d2ddb1d2f
Egor Kazantsev <3gork4z4ncev@gmail.com>
2022-05-20 20:46:41 +0500

    empty fragments

---
+++ app/src/main/java/org/egorkazantsev/dekonmobile/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.egorkazantsev.dekonmobile.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.egorkazantsev.dekonmobile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
