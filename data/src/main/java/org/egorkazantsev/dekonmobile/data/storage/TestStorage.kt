5a5c065392fd5f84e77fe1cee9b4f87a5f4487fb
Egor Kazantsev <3gork4z4ncev@gmail.com>
2022-05-30 04:02:43 +0500

    smt

--- data/src/main/java/org/egorkazantsev/dekonmobile/data/storage/TestStorage.kt
+++ data/src/main/java/org/egorkazantsev/dekonmobile/data/storage/TestStorage.kt
@@ -8,10 +8,10 @@ import java.util.*
 
 val models = listOf(
     Model(
-        UUID.fromString("e015cd30-f085-49d8-b186-3475d86f1e2d"), "model 2 crt",
+        UUID.fromString("e015cd30-f085-49d8-b186-3475d86f1e2d"), "Модель 2 крит",
         Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
         Matrix(
-            UUID.randomUUID(), "main matrix", 2.4,
+            UUID.randomUUID(), "Главная матрица", 2.4,
             leftElement = Criteria(
                 UUID.randomUUID(), "crt 1", 1.9
             ),
@@ -21,7 +21,7 @@ val models = listOf(
         )
     ),
     Model(
-        UUID.fromString("765ad7c9-05aa-4a71-a76a-003298ad4b81"), "model 1 mtr 3 crt",
+        UUID.fromString("765ad7c9-05aa-4a71-a76a-003298ad4b81"), "Model 1 mtr 3 crt",
         Owner(UUID.randomUUID(), "Зубенко Михаил Петрович"),
         Matrix(
             UUID.randomUUID(), "main matrix", 2.4,
@@ -36,6 +36,116 @@ val models = listOf(
                 rightElement = Criteria(
                     UUID.randomUUID(), "crt 3", 1.4
                 )
+            )
+        )
+    ),
+    Model(
+        UUID.randomUUID(), "Качество работы управляющей компании",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "Качество управления МКД", 1.0,
+            leftElement = Matrix(
+                UUID.randomUUID(), "Критерий оценки качества предоставляемой информации", 1.0,
+                leftElement = Matrix(
+                    UUID.randomUUID(), "Предоставление и доступность информации", 1.0,
+                    leftElement = Criteria(
+                        UUID.randomUUID(), "Предоставление информации о перечне услуг, их стоимости", 1.0
+                    ),
+                    rightElement = Criteria(
+                        UUID.randomUUID(), "Доступность информации", 1.0
+                    )
+                ),
+                rightElement = Matrix(
+                    UUID.randomUUID(), "Взаимодействие с потребителями", 1.0,
+                    leftElement = Criteria(
+                        UUID.randomUUID(), "Предоставление годового плана работ и полного отчета об исполнении плана", 1.0
+                    ),
+                    rightElement = Criteria(
+                        UUID.randomUUID(), "Взаимодействие с собственниками помещений для получения обратной связи", 1.0
+                    )
+                )
+            ),
+            rightElement = Matrix(
+                UUID.randomUUID(), "Критерий оценки качества услуг и рабор упр. орг.", 1.0,
+                leftElement = Matrix(
+                    UUID.randomUUID(), "Обслуживание МКД", 1.0,
+                    leftElement = Criteria(
+                        UUID.randomUUID(), "Содержание и ремон общего имущества МКД", 1.0
+                    ),
+                    rightElement = Criteria(
+                        UUID.randomUUID(), "Предоставление коммунальных услуг", 1.0
+                    )
+                ),
+                rightElement = Matrix(
+                    UUID.randomUUID(), "Работа ресурсоснабжающими организациями", 1.0,
+                    leftElement = Criteria(
+                        UUID.randomUUID(), "Подписание договоров с РСО, контроль качества услуг, сбор средств и регулярная оплата полученных услуг", 1.0
+                    ),
+                    rightElement = Criteria(
+                        UUID.randomUUID(), "Разработка и прим. мероприятий по энергоснабжению и повышению энергической эффективности МКД", 1.0
+                    )
+                )
+            )
+        )
+    ),
+    Model(
+        UUID.randomUUID(), "Качество торгово-развлекательного комплекса",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "main matrix", 2.4,
+            leftElement = Criteria(
+                UUID.randomUUID(), "crt 1", 1.9
+            ),
+            rightElement = Criteria(
+                UUID.randomUUID(), "crt 2", 2.1,
+            )
+        )
+    ),Model(
+        UUID.randomUUID(), "Привлекательность арендатора для объекта",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "main matrix", 2.4,
+            leftElement = Criteria(
+                UUID.randomUUID(), "crt 1", 1.9
+            ),
+            rightElement = Criteria(
+                UUID.randomUUID(), "crt 2", 2.1,
+            )
+        )
+    ),Model(
+        UUID.randomUUID(), "Качество предоставления услуг по содержанию здания МКД и его придомовой площади",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "main matrix", 2.4,
+            leftElement = Criteria(
+                UUID.randomUUID(), "crt 1", 1.9
+            ),
+            rightElement = Criteria(
+                UUID.randomUUID(), "crt 2", 2.1,
+            )
+        )
+    ),Model(
+        UUID.randomUUID(), "Надежность застройщика",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "main matrix", 2.4,
+            leftElement = Criteria(
+                UUID.randomUUID(), "crt 1", 1.9
+            ),
+            rightElement = Criteria(
+                UUID.randomUUID(), "crt 2", 2.1,
+            )
+        )
+    ),Model(
+        UUID.randomUUID(), "Оценка компании на банкротство",
+        Owner(UUID.randomUUID(), "Зарплатин Пупа Лупович"),
+        Matrix(
+            UUID.randomUUID(), "main matrix", 2.4,
+            leftElement = Criteria(
+                UUID.randomUUID(), "crt 1", 1.9
+            ),
+            rightElement = Criteria(
+                UUID.randomUUID(), "crt 2", 2.1,
             )
         )
     )
