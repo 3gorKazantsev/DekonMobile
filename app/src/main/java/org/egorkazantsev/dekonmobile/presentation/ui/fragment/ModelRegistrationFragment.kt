fa3676b7b0cd33e1beddb5f1f35191826417b975
Egor Kazantsev <3gork4z4ncev@gmail.com>
2022-05-30 02:59:48 +0500

    ModelRegistrationFragment.kt

--- app/src/main/java/org/egorkazantsev/dekonmobile/presentation/ui/fragment/ModelRegistrationFragment.kt
+++ app/src/main/java/org/egorkazantsev/dekonmobile/presentation/ui/fragment/ModelRegistrationFragment.kt
@@ -1,5 +1,6 @@
 package org.egorkazantsev.dekonmobile.presentation.ui.fragment
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import org.egorkazantsev.dekonmobile.R
 import org.egorkazantsev.dekonmobile.databinding.FragmentModelRegistrationBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ModelRegistrationFragment : Fragment() {
 
@@ -15,6 +18,8 @@ class ModelRegistrationFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args: ModelRegistrationFragmentArgs by navArgs()
+
+    private val calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +30,40 @@ class ModelRegistrationFragment : Fragment() {
         // изменение заголовка экшн бара
         activity?.title = getString(R.string.model_registration)
 
+        val date = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            calendar.apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DAY_OF_MONTH, day)
+                updateText()
+            }
+        }
+
+        // установка наименования и ФИО
+        binding.apply {
+            modelNameEditText.setText(args.modelName)
+            fullNameEditText.setText(args.ownerName)
+        }
+
+        // слушатели
+        binding.apply {
+            birthDateEditText.setOnClickListener {
+                DatePickerDialog(
+                    requireContext(),
+                    date,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+
         return binding.root
+    }
+
+    private fun updateText() {
+        val format = "MM.dd.yyyy"
+        val dateFormat = SimpleDateFormat(format, Locale.getDefault())
+        binding.birthDateEditText.setText(dateFormat.format(calendar.time))
     }
 }
